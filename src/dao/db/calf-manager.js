87bb156a5a86b8c47d7499c63de52cdb05e694b3
Agustin Lorenzatti <lorenzatiro@gmail.com>
2024-04-14 19:35:11 -0300

    correcion

--- src/dao/db/calf-manager.js
+++ src/dao/db/calf-manager.js
@@ -1,15 +1,26 @@
 const CalfModel = require("../models/calf.model.js");
-const moment = require("moment-timezone")
+const moment = require("moment-timezone");
 
 class CalfManager {
-  async addCalf({ name, treatment, startDate, owner, endDate, duration, medication, corral, corralId, lastDayTreated}) {
+  async addCalf({
+    name,
+    treatment,
+    startDate,
+    owner,
+    endDate,
+    duration,
+    medication,
+    corral,
+    corralId,
+    lastDayTreated,
+  }) {
     try {
       if (!name || !treatment || !startDate) {
         console.log("Todos los campos son obligatorios");
         return;
       }
 
-      let calfExist = await CalfModel.findOne({owner:owner, name:name})
+      let calfExist = await CalfModel.findOne({ owner: owner, name: name });
 
       if (calfExist) {
         calfExist.treatment = treatment;
@@ -19,7 +30,7 @@ class CalfManager {
         calfExist.medication = medication;
         calfExist.corral = corral;
         calfExist.corralId = corralId;
-        calfExist.lastDayTreated = lastDayTreated
+        calfExist.lastDayTreated = lastDayTreated;
         await calfExist.save();
         console.log("Ternero actualizado:", calfExist);
       } else {
@@ -34,7 +45,7 @@ class CalfManager {
           owner,
           corral,
           corralId,
-          lastDayTreated
+          lastDayTreated,
         });
         await newCalf.save();
         console.log("Nuevo ternero creado:", newCalf);
@@ -46,7 +57,7 @@ class CalfManager {
 
   async getCalves(userId) {
     try {
-      const calves = await CalfModel.find({owner: userId});
+      const calves = await CalfModel.find({ owner: userId });
       return calves;
     } catch (error) {
       console.log(error);
@@ -57,96 +68,112 @@ class CalfManager {
     try {
       const startOfToday = moment(today).startOf("day");
 
-      const activeCalves = await CalfModel.find({owner:userId, endDate: {$gte:today}})
+      const activeCalves = await CalfModel.find({
+        owner: userId,
+        endDate: { $gte: today },
+      });
 
-      const notTreatedToday = activeCalves.filter(calf => {
-        
-        return moment(calf.lastDayTreated).startOf("day").isBefore(startOfToday);
-    });
+      const notTreatedToday = activeCalves.filter((calf) => {
+        return moment(calf.lastDayTreated)
+          .startOf("day")
+          .isBefore(startOfToday);
+      });
 
-      return notTreatedToday
+      return notTreatedToday;
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
-
 
   async getActiveCalvesTreated(userId, today) {
     try {
       const startOfToday = moment(today).startOf("day");
 
-      const activeCalves = await CalfModel.find({owner:userId, endDate: {$gte:today}})
+      const activeCalves = await CalfModel.find({
+        owner: userId,
+        endDate: { $gte: today },
+      });
 
-      const notTreatedToday = activeCalves.filter(calf => {
-        
-        return moment(calf.lastDayTreated).startOf("day").isSame(startOfToday);
-    });
-
-     
-    return notTreatedToday
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  async getYesterdayCalves (userId, yesterday) {
-
-    const startOfYesterday = new Date(yesterday)
-    startOfYesterday.setHours(0,0,0,0)
-    const endOfYesterday = new Date(yesterday)
-    endOfYesterday.setHours(23, 59,59, 999)
-    
-  
-    try {   
-      const yesterdayCalves = await CalfModel.find({owner:userId, endDate:{ $gte: startOfYesterday, $lte:endOfYesterday}})
-      
-      return yesterdayCalves
-    } catch (error) {
-      console.log(error)    
-    }
-  }
-
-  async getCalvesByCorral(userId, corral, today){
-    try {
-      const calvesInCorral = await CalfModel.find({owner:userId, corralId: corral, endDate: {$gte:today}})
-      return calvesInCorral
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-
-  async getCalvesByCorralNotTreated(userId, corral, today){
-    try {
-      const startOfToday = moment(today).startOf("day");
-
-      const activeCalves = await CalfModel.find({owner:userId, corralId: corral, endDate: {$gte:today}})
-
-      const calvesNotTreatedTodayInCorral = activeCalves.filter(calf => {
-        
-        return moment(calf.lastDayTreated).startOf("day").isBefore(startOfToday);
-    });
-
-      return calvesNotTreatedTodayInCorral
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  async getCalvesByCorralTreated(userId, corral, today){
-    try {
-      const startOfToday = moment(today).startOf("day");
-  
-        const activeCalves = await CalfModel.find({owner:userId, corralId: corral, endDate: {$gte:today}})
-  
-        const calvesTreatedTodayInCorral = activeCalves.filter(calf => {
-          
+      const notTreatedToday = activeCalves.filter((calf) => {
         return moment(calf.lastDayTreated).startOf("day").isSame(startOfToday);
       });
-  
-        return calvesTreatedTodayInCorral
+
+      return notTreatedToday;
     } catch (error) {
-      console.log(error)
+      console.log(error);
+    }
+  }
+
+  async getYesterdayCalves(userId, yesterday) {
+    const startOfYesterday = new Date(yesterday);
+    startOfYesterday.setHours(0, 0, 0, 0);
+    const endOfYesterday = new Date(yesterday);
+    endOfYesterday.setHours(23, 59, 59, 999);
+
+    try {
+      const yesterdayCalves = await CalfModel.find({
+        owner: userId,
+        endDate: { $gte: startOfYesterday, $lte: endOfYesterday },
+      });
+
+      return yesterdayCalves;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getCalvesByCorral(userId, corral, today) {
+    try {
+      const calvesInCorral = await CalfModel.find({
+        owner: userId,
+        corralId: corral,
+        endDate: { $gte: today },
+      });
+      return calvesInCorral;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getCalvesByCorralNotTreated(userId, corral, today) {
+    try {
+      const startOfToday = moment(today).startOf("day");
+
+      const activeCalves = await CalfModel.find({
+        owner: userId,
+        corralId: corral,
+        endDate: { $gte: today },
+      });
+
+      const calvesNotTreatedTodayInCorral = activeCalves.filter((calf) => {
+        return moment(calf.lastDayTreated)
+          .startOf("day")
+          .isBefore(startOfToday);
+      });
+
+      return calvesNotTreatedTodayInCorral;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getCalvesByCorralTreated(userId, corral, today) {
+    try {
+      const startOfToday = moment(today).startOf("day");
+
+      const activeCalves = await CalfModel.find({
+        owner: userId,
+        corralId: corral,
+        endDate: { $gte: today },
+      });
+
+      const calvesTreatedTodayInCorral = activeCalves.filter((calf) => {
+        return moment(calf.lastDayTreated).startOf("day").isSame(startOfToday);
+      });
+
+      return calvesTreatedTodayInCorral;
+    } catch (error) {
+      console.log(error);
     }
   }
   async getCalfById(id) {
@@ -194,16 +221,15 @@ class CalfManager {
     }
   }
 
-
   async markAsTreated(calfId) {
     try {
       const calf = await CalfModel.findById(calfId);
-      calf.lastDayTreated = moment();
+      calf.lastDayTreated = moment.tz("America/Argentina/Buenos_Aires");
       const updatedCalf = await calf.save();
 
-    return updatedCalf;
+      return updatedCalf;
     } catch (error) {
-     console.log(error) 
+      console.log(error);
     }
   }
 }
